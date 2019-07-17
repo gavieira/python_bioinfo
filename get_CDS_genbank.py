81e68212e320503c97c7f4f6a4db35cd3ca00498
gavieira <gabrieldeusdeth@gmail.com>
2019-07-17 11:31:52 -0300

    Fixed 'get_CDS_genbank.py'

--- get_CDS_genbank.py
+++ get_CDS_genbank.py
@@ -2,10 +2,15 @@
 # coding: utf-8
 
 from Bio import SeqIO
-import re, sys
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description="This script extracts CDS sequences from genbank files")
+parser.add_argument("genbank", type=str, metavar="genbank", nargs='*', help="Genbank file(s)")
+args = parser.parse_args()
 
 def write_CDSs_to_file():
-    for i in sys.argv[1:]:
+    for i in args.genbank:
         with open ("{}.CDS.fa".format(i.rsplit(".", 1)[0]), "w") as CDS:
             coding_list = extract_CDS("Allenopithecus_nigroviridis_NC_023965.1.gb")
             for i in coding_list:
@@ -17,13 +22,12 @@ def extract_CDS(genbank):
         if record.features:
             for FEATURE in record.features:
                 if FEATURE.type == "CDS":
-                    raw_header = FEATURE.qualifiers.get("gene")
-                    formatted_header = re.search("\[\'(\S+)\'\]", str(raw_header))
-                    CDS_list.append(">{}\n{}\n".format(formatted_header.group(1), FEATURE.location.extract(record).seq))
+                    header = FEATURE.qualifiers.get("gene")[0]
+                    sequence = FEATURE.location.extract(record).seq
+                    CDS_list.append(">{}\n{}\n".format(header, sequence))
     return(CDS_list)
 
-
-write_CDSs_to_file()
-
-
-
+if __name__ == "__main__":
+    write_CDSs_to_file()
+    if len(args.genbank) == 0:
+        parser.print_help()
